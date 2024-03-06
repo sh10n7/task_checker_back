52366d949ff64a476a5449b1e5db2558167257fa
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-06 12:06:45 +0900

    seed.jsの更新

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,19 +3,27 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ジャンルデータの作成
-  await prisma.genre.createMany({
-    data: [
-      { name: "sample1" },
-      { name: "sample2" },
-    ],
-  });
+  // ジャンルデータが存在しない場合は、デフォルトデータを保存する。
+  const genresCount = await prisma.genre.count();
+  if(genresCount === 0) {
+    await prisma.genre.createMany({
+      data: [
+        { name: "sample1" },
+        { name: "sample2" },
+      ],
+    });
+  }
   // タスクデータの作成
-  await prisma.task.createMany({
-    data: [
-      { name: "task1", explanation: "test1", deadlineDate: new Date("2024-01-01"), status: 0, genreId: 1 },
-      { name: "task2", explanation: "test2", deadlineDate: new Date("2024-01-02"), status: 0, genreId: 2 },
-    ],
-  });
+  // タスクデータが存在しない場合は、デフォルトデータを保存する。
+  const tasksCount = await prisma.task.count();
+  if(tasksCount === 0) {
+    await prisma.task.createMany({
+      data: [
+        { name: "task1", explanation: "test1", deadlineDate: new Date("2024-01-01"), status: 0, genreId: 1 },
+        { name: "task2", explanation: "test2", deadlineDate: new Date("2024-01-02"), status: 0, genreId: 2 },
+      ],
+    });
+  }
 }
 
 main()
